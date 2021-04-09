dcd0cf94523dcbdf4a1d6df952108095d5033459
Arlete_Sousa <81167437+Arlete-sousa@users.noreply.github.com>
2021-04-09 20:41:45 -0300

    Olá, Mundo!
    Primeiro programa em java

---
+++ PrimeiroPrograma.java
@@ -0,0 +1,9 @@
+package primeiroprograma; /*não é obrigatório*/
+
+public class PrimeiroPrograma {
+    public static void main(String[] args){
+        System.out.println("Olá, Mundo!");/*o println desce um linha*/
+         /*sout + teclaTAB */  
+    }
+   
+} 

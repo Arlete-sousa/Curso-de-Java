1f1d566bc458d30d9958f71ccbb9ff97f7ef61b1
Arlete_Sousa <81167437+Arlete-sousa@users.noreply.github.com>
2021-04-10 15:49:49 -0300

    HoraDoSistema
    Programa que mostra a hora do sistema.

---
+++ HoraDoSistemaa.java
@@ -0,0 +1,22 @@
+
+package horadosistemaa;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Arlete
+ */
+public class HoraDoSistemaa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Date relogio = new Date();
+        System.out.println("A hora do sistema é:");
+        System.out.println(relogio.toString());
+    }
+    
+}
